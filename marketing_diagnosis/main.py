e52e13cf399b2f03b9badea7825c1986d64822e7
lijianlan1115-crypto <lijianlan1115@gmail.com>
2026-07-14 16:16:37 +0800

    Route CLI through corrected database loader

--- marketing_diagnosis/main.py
+++ marketing_diagnosis/main.py
@@ -10,9 +10,9 @@ from typing import Any
 
 from marketing_diagnosis.data_v2 import normalize_dataset
 from marketing_diagnosis.excel_loader import load_excel_dataset
-from marketing_diagnosis.db_loader import load_database_dataset, load_mysql_dsn_dataset
+from marketing_diagnosis.db_loader_v2 import load_database_dataset, load_mysql_dsn_dataset
 from marketing_diagnosis.reporting_v2 import write_reports
-from marketing_diagnosis.rules_v2 import process
+from marketing_diagnosis.rules_v3 import process
 
 DEFAULT_REPORT_ROOT = Path("/var/lib/ota-marketing-diagnosis/reports")
 
