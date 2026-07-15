68e942184da1d9b4d59cf30e9c07edb0d5a04c85
lijianlan1115-crypto <lijianlan1115@gmail.com>
2026-07-15 15:26:08 +0800

    Use compact report without subtotal row and scope selector

--- marketing_diagnosis/reporting_v7.py
+++ marketing_diagnosis/reporting_v7.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from marketing_diagnosis.reporting_v21 import build_html, build_markdown, write_reports
+from marketing_diagnosis.reporting_v22 import build_html, build_markdown, write_reports
 
 __all__ = ["build_html", "build_markdown", "write_reports"]
