895c4f4d32969db73f33371d1cf245fa6f4f0333
lijianlan1115-crypto <lijianlan1115@gmail.com>
2026-07-15 14:51:53 +0800

    Use non-truncating final report renderer

--- marketing_diagnosis/reporting_v7.py
+++ marketing_diagnosis/reporting_v7.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from marketing_diagnosis.reporting_v19 import build_html, build_markdown, write_reports
+from marketing_diagnosis.reporting_v20 import build_html, build_markdown, write_reports
 
 __all__ = ["build_html", "build_markdown", "write_reports"]
