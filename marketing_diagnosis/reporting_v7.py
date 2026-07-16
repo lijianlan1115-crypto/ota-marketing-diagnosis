a9fd01251556e18538abf89e9b1d3cfa5189c5c7
lijianlan1115-crypto <lijianlan1115@gmail.com>
2026-07-16 11:37:39 +0800

    Use HOS-style information score report layout

--- marketing_diagnosis/reporting_v7.py
+++ marketing_diagnosis/reporting_v7.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from marketing_diagnosis.reporting_v23 import build_html, build_markdown, write_reports
+from marketing_diagnosis.reporting_v24 import build_html, build_markdown, write_reports
 
 __all__ = ["build_html", "build_markdown", "write_reports"]
