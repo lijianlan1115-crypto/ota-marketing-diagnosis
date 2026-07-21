f1d489a4112f0f1fafad39580c24048bee8647d4
lijianlan1115-crypto <lijianlan1115@gmail.com>
2026-07-21 15:14:22 +0800

    feat: export dual-channel HTML builder

--- marketing_diagnosis/reporting_v2.py
+++ marketing_diagnosis/reporting_v2.py
@@ -2,9 +2,18 @@ from __future__ import annotations
 
 from marketing_diagnosis.reporting_runtime_v52 import (
     build_ctrip_html,
+    build_dual_channel_html,
     build_html,
     build_markdown,
+    build_meituan_html,
     write_reports,
 )
 
-__all__ = ["build_ctrip_html", "build_html", "build_markdown", "write_reports"]
+__all__ = [
+    "build_ctrip_html",
+    "build_dual_channel_html",
+    "build_html",
+    "build_markdown",
+    "build_meituan_html",
+    "write_reports",
+]
