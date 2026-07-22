e924bd147ad5727198a0101ac88ed7b0d1d3abc5
Lemonsjh <127745304+Lemonsjh@users.noreply.github.com>
2026-07-22 14:26:07 +0800

    style: enable flow scoring explanation

--- marketing_diagnosis/reporting_v2.py
+++ marketing_diagnosis/reporting_v2.py
@@ -9,6 +9,7 @@ from marketing_diagnosis.reporting_runtime_v52 import (
     build_meituan_html,
     write_reports,
 )
+from marketing_diagnosis import ctrip_flow_scoring_note as _ctrip_flow_scoring_note
 from marketing_diagnosis import ctrip_psi_table_compact as _ctrip_psi_table_compact
 from marketing_diagnosis import ctrip_rights_center_compact as _ctrip_rights_center_compact
 
